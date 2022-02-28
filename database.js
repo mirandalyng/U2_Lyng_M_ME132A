595775017c92bc441718e1229f4771c25f31b589
mirandalyng <lyngmiranda@gmail.com>
2022-02-28 09:29:47 +0100

    JS, HTML, CSS-file
    Created the JS, HTML, CSS-files for the project, and conected them to each other.
    
    Created the array of object.

---
+++ database.js
@@ -0,0 +1,85 @@
+// Our database of artist
+"use strict";
+
+let databaseOfArtist= [
+    {  
+        id: 1,
+        name: "Dua Lipa",
+        genre: "Pop",
+        age:   26, 
+        country: "England",
+    },
+
+    {
+        id: 2,
+        name: "Bob Dylan",
+        genre: "Blues",
+        age: 80,
+        country: "USA",
+    },
+
+    {
+        id: 3,
+        name: "Peggy Gou",
+        genre: "Deep House", 
+        age: 30, 
+        country: "South Korea",
+    },
+
+    {
+        id: 4,
+        name: "Kanye West",
+        genre: "Hip Hop",
+        age: 44,
+        country: "USA",
+    }, 
+
+    {
+        id: 5,
+        name: "Kate Bush",
+        genre: "Pop Rock",
+        age: 63, 
+        Country:"England",
+    },
+    
+    {
+        id: 6,
+        name: "Kornél Kovács",
+        genre: "Electronic",
+        age: 36,
+        country: "Sweden",
+    },
+
+    {
+        id: 7,
+        name: "FKA twigs",
+        genre: "Trip Hop",
+        age: 34,
+        country: "England",
+    },
+    
+    {
+        id: 8,
+        name: "Mall Grab",
+        genre: "Electro",
+        age: 28,
+        country: "Australia",
+    },
+
+    {
+        id: 9,
+        name: "James Blake",
+        genre: "Electronic",
+        age: 33,
+        country: "England",
+
+    },
+    {
+        id: 10,
+        name: "SZA",
+        genre: "R&B",
+        age: 32,
+        country: "USA",
+    }
+
+];
