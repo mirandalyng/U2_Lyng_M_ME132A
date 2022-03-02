c8365547b7dc55ca5a913bd60ffcd4a422c20167
mirandalyng <lyngmiranda@gmail.com>
2022-03-02 12:12:01 +0100

    Finishing touthches
    Renamed the Show-all function to resetall because that's what the button says, and it makes it clearer.
    
    function onResetFilter(){
    
    clickResetAndFilter(){
    
    And did some finishing in the CSS och HTML-files.
    
    Also formated all the JS, HTML and CSS by using Prettier - Code formatter.

--- database.js
+++ database.js
@@ -1,85 +1,83 @@
 //Database of artist
 "use strict";
 
-let databaseOfArtist= [
-    {  
-        id: 1,
-        name: "Dua Lipa",
-        genre: "Pop",
-        age:   26, 
-        country: "England",
-    },
+let databaseOfArtist = [
+  {
+    id: 1,
+    name: "Dua Lipa",
+    genre: "Pop",
+    age: 26,
+    country: "England",
+  },
 
-    {
-        id: 2,
-        name: "Bob Dylan",
-        genre: "Blues",
-        age: 80,
-        country: "USA",
-    },
+  {
+    id: 2,
+    name: "Bob Dylan",
+    genre: "Blues",
+    age: 80,
+    country: "USA",
+  },
 
-    {
-        id: 3,
-        name: "Peggy Gou",
-        genre: "Deep House", 
-        age: 30, 
-        country: "South Korea",
-    },
+  {
+    id: 3,
+    name: "Peggy Gou",
+    genre: "Deep House",
+    age: 30,
+    country: "South Korea",
+  },
 
-    {
-        id: 4,
-        name: "Kanye West",
-        genre: "Hip Hop",
-        age: 44,
-        country: "USA",
-    }, 
+  {
+    id: 4,
+    name: "Kanye West",
+    genre: "Hip Hop",
+    age: 44,
+    country: "USA",
+  },
 
-    {
-        id: 5,
-        name: "Kate Bush",
-        genre: "Pop Rock",
-        age: 63, 
-        country:"England",
-    },
-    
-    {
-        id: 6,
-        name: "Kornél Kovács",
-        genre: "Electronic",
-        age: 36,
-        country: "Sweden",
-    },
+  {
+    id: 5,
+    name: "Kate Bush",
+    genre: "Pop Rock",
+    age: 63,
+    country: "England",
+  },
 
-    {
-        id: 7,
-        name: "FKA twigs",
-        genre: "Trip Hop",
-        age: 34,
-        country: "England",
-    },
-    
-    {
-        id: 8,
-        name: "Mall Grab",
-        genre: "Electro",
-        age: 28,
-        country: "Australia",
-    },
+  {
+    id: 6,
+    name: "Kornél Kovács",
+    genre: "Electronic",
+    age: 36,
+    country: "Sweden",
+  },
 
-    {
-        id: 9,
-        name: "James Blake",
-        genre: "Electronic",
-        age: 33,
-        country: "England",
+  {
+    id: 7,
+    name: "FKA twigs",
+    genre: "Trip Hop",
+    age: 34,
+    country: "England",
+  },
 
-    },
-    {
-        id: 10,
-        name: "SZA",
-        genre: "R&B",
-        age: 32,
-        country: "USA",
-    }
+  {
+    id: 8,
+    name: "Mall Grab",
+    genre: "Electro",
+    age: 28,
+    country: "Australia",
+  },
 
+  {
+    id: 9,
+    name: "James Blake",
+    genre: "Electronic",
+    age: 33,
+    country: "England",
+  },
+  {
+    id: 10,
+    name: "SZA",
+    genre: "R&B",
+    age: 32,
+    country: "USA",
+  },
 ];
--- main.js
+++ main.js
@@ -1,93 +1,72 @@
 "use strict";
 
+//Function to create a new artist.
 
-//Function to create a new artist. 
+function createNewArtist(name, genre, age, country) {
+  let newArtist = {
+    name: name,
+    genre: genre,
+    age: age,
+    country: country,
+  };
 
-function createNewArtist(name, genre, age, country){
+  return newArtist;
+}
 
-    let newArtist = { 
-        name: name, 
-        genre: genre,
-        age: age,
-        country: country,
+//Function to add artist to the database of other artists
 
+function addArtistToDatabase(database, artist) {
+  database.push(artist);
+}
+
+//Function to remove an artist by ID from the database.
+
+function removeArtistById(artists, id) {
+  for (let i = 0; i < artists.length; i++) {
+    let artist = artists[i];
+
+    if (artist.id == id) {
+      artists.splice(i, 1);
+
+      return;
     }
-
-    return newArtist;
+  }
 }
-
-
-//Function to add it to the database of other artists
-
-function addArtistToDatabase(database, artist){
-    database.push(artist);
-}
-
-
-//Function to remove an artist by ID from the database. 
-
-function removeArtistById(artists, id){
-    
-    for(let i = 0; i < artists.length; i++){
-        let artist = artists[i]
-    
-
-        if(artist.id == id){
-            artists.splice(i,1);
-
-            return; 
-
-        }
-
-    
-    
-    }
-}
-
 
 //Two functions to get artists by using genre and country. Theese will be used in the
-// filtering process later on in the code. 
+// filtering process later on in the code.
 
-function getArtistByGenre(artists, genre){
+function getArtistByGenre(artists, genre) {
+  let artistByGenre = [];
 
-    let artistByGenre = [];
-
-    for(let artist of artists){
-        if(artist.genre.toLowerCase() == genre.toLowerCase()){
-        artistByGenre.push(artist);
-        }
+  for (let artist of artists) {
+    if (artist.genre.toLowerCase() == genre.toLowerCase()) {
+      artistByGenre.push(artist);
     }
+  }
 
-    return artistByGenre;
-
+  return artistByGenre;
 }
 
+function getArtistByCountry(artists, country) {
+  let artistByCountry = [];
 
-function getArtistByCountry(artists, country){
-    
-   let artistByCountry = [];
-
-        for(let artist of artists){
-            if(artist.country.toLowerCase() == country.toLowerCase()){
-            artistByCountry.push(artist);
-        }
-
+  for (let artist of artists) {
+    if (artist.country.toLowerCase() == country.toLowerCase()) {
+      artistByCountry.push(artist);
     }
+  }
 
-        return artistByCountry;
+  return artistByCountry;
 }
 
+//Function to make a new div in HTML for the artist created by createNewArtist - function.
+function makeArtist(artist) {
+  let div = document.createElement("div");
+  div.classList.add("artist");
+  div.id = artist.id;
 
-
-//Function to make a new div in HTML for the artist created by createNewArtist - function. 
-function makeArtist(artist){
-    let div = document.createElement("div");
-    div.classList.add("artist");
-    div.id = artist.id;
-
-
-
-    div.innerHTML = `
+  div.innerHTML = `
     <li>${artist.name}</li>
     <div>${artist.genre}</div>
     <div>${artist.age}</div>
@@ -95,155 +74,127 @@ function makeArtist(artist){
     <button type = "button">Remove</div>
     `;
 
-    return div;
+  return div;
 }
-
 
 //Funciton to make the array of artists into HTML
-function makeArtists(artists){
-    let artistsId = document.getElementById("artists");
-    artistsId.innerHTML = "";
+function makeArtists(artists) {
+  let artistsId = document.getElementById("artists");
+  artistsId.innerHTML = "";
 
-    for(let artist of artists){
-        let artistId = makeArtist(artist);
-        artistsId.appendChild(artistId);
-    }
+  for (let artist of artists) {
+    let artistId = makeArtist(artist);
+    artistsId.appendChild(artistId);
+  }
 
-    clickRemoveArtist();
+  clickRemoveArtist();
 }
-
-
-
 
 //Functions that are going to work together with the buttonClicks
 
 //The submitting of all the information about the new created artist. Going to work with clickNewArtist()
-function submitArtistInfo(event){
+function submitArtistInfo(event) {
+  event.preventDefault();
 
-    event.preventDefault();
+  let name = document.getElementById("name").value;
+  let genre = document.getElementById("genre").value;
+  let age = Number(document.getElementById("age").value);
+  let country = document.getElementById("country").value;
 
-    let name = document.getElementById("name").value;
-    let genre = document.getElementById("genre").value;
-    let age = Number(document.getElementById("age").value);
-    let country = document.getElementById("country").value;
+  if (name == "") {
+    return alert("Fill in all the information please.");
+  } else if (genre == "") {
+    return alert("Fill in all the information please.");
+  } else if (age == 0) {
+    return alert("Fill in all the information please.");
+  } else if (country == "") {
+    return alert("Fill in all the information please.");
+  }
 
-     if(name == ""){
-        return alert("Fill in all the information please.");
-    }
+  let artist = createNewArtist(name, genre, age, country);
 
-    else if(genre == ""){
-        return alert("Fill in all the information please.");
-    }
+  artist.id = databaseOfArtist[databaseOfArtist.length - 1].id + 1;
 
-    else if(age == 0){
-        return alert("Fill in all the information please.");
-    }
+  addArtistToDatabase(databaseOfArtist, artist);
+  makeArtists(databaseOfArtist);
 
-    else if(country == ""){
-       
-        return alert("Fill in all the information please.");
-        
-    }
-   
+  let emptyForm = document.getElementById("create-new-artist");
 
-    let artist = createNewArtist(name, genre, age, country);
-
-    artist.id = databaseOfArtist[databaseOfArtist.length -1].id + 1; 
-
-    addArtistToDatabase(databaseOfArtist,artist);
-    makeArtists(databaseOfArtist);
-
-    let emptyForm = document.getElementById("create-new-artist");
-    
-    emptyForm.reset();
+  emptyForm.reset();
 }
 
+//On the filter based on genre that is going to run in the clickResetAllAndFilter
 
+function onFilterByGenre(event) {
+  event.preventDefault();
 
-//On the filter based on genre that is going to run in the clickShowAllAndFilter
+  let genre = document.getElementById("filter-genre").value;
 
-function onFilterByGenre(event){
-    event.preventDefault();
+  let artists = getArtistByGenre(databaseOfArtist, genre);
 
-    let genre = document.getElementById("filter-genre").value;
-     
-    let artists = getArtistByGenre(databaseOfArtist, genre);
-    
-    makeArtists(artists);
-
+  makeArtists(artists);
 }
-
 
 //On the filter based on country that is going to run in the clickShowAllAndFilter
-function onFilterByCountry(event){
-    event.preventDefault();
+function onFilterByCountry(event) {
+  event.preventDefault();
 
-    let country = document.getElementById("filter-country").value;
+  let country = document.getElementById("filter-country").value;
 
-    let artists = getArtistByCountry(databaseOfArtist, country);
-    makeArtists(artists);
+  let artists = getArtistByCountry(databaseOfArtist, country);
+  makeArtists(artists);
 }
 
-//Function for on the show all button that is going to run in the clickShowAllAndFilter
-function onShowAll(){
-    document.getElementById("filter-genre").value = "";
-    document.getElementById("filter-country").value = "";
-    makeArtists(databaseOfArtist);
+//Function for on the show all button that is going to run in the clickResetAllAndFilter
+function onResetFilter() {
+  document.getElementById("filter-genre").value = "";
+  document.getElementById("filter-country").value = "";
+  makeArtists(databaseOfArtist);
 }
 
+// the function for all the remove-buttons.
+function onRemoveButtons(event) {
+  let button = event.target;
+  let id = button.parentElement.id;
 
+  if (confirm("Are you sure you want to delete this artist?") == true) {
+    removeArtistById(databaseOfArtist, id);
+  } else return false;
 
-// the function for all the remove-buttons. 
-function onRemoveButtons(event){
-    let button = event.target;
-    let id = button.parentElement.id;
+  makeArtists(databaseOfArtist);
 
-    if(confirm("Are you sure you want to delete this artist?")== true){
-        removeArtistById(databaseOfArtist,id)
-    } else return false;
-
-    makeArtists(databaseOfArtist);
-
-    clickRemoveArtist();
+  clickRemoveArtist();
 }
 
+//Button click functions
 
-//Button click functions 
-
-//Function for createNewArtist 
-function clickAddNewArtist(){
-    let form = document.getElementById("create-new-artist");
-    form.addEventListener("submit",submitArtistInfo);
-    
+//Function for createNewArtist
+function clickAddNewArtist() {
+  let form = document.getElementById("create-new-artist");
+  form.addEventListener("submit", submitArtistInfo);
 }
 
-//Function for clicking the remove-buttons 
-function clickRemoveArtist(){
-    let buttons = document.querySelectorAll(".artist button");
-    
-    for (let button of buttons){
-        button.addEventListener("click", onRemoveButtons);
-    }
-}
+//Function for clicking the remove-buttons
+function clickRemoveArtist() {
+  let buttons = document.querySelectorAll(".artist button");
 
+  for (let button of buttons) {
+    button.addEventListener("click", onRemoveButtons);
+  }
+}
 
 //function for the filter buttons
-function clickShowAllAndFilter(){
-    let inputForGenre = document.getElementById("filter-by-genre");
-    let inputForCountry = document.getElementById("filter-by-country");
-    let showAll = document.getElementById("show-all");
+function clickResetAndFilter() {
+  let inputForGenre = document.getElementById("filter-by-genre");
+  let inputForCountry = document.getElementById("filter-by-country");
+  let resetAll = document.getElementById("reset-all");
 
-    inputForGenre.addEventListener("submit", onFilterByGenre);
-    inputForCountry.addEventListener("submit", onFilterByCountry);
-    showAll.addEventListener("click", onShowAll);
+  inputForGenre.addEventListener("submit", onFilterByGenre);
+  inputForCountry.addEventListener("submit", onFilterByCountry);
+  resetAll.addEventListener("click", onResetFilter);
 }
 
-
-
-
-
-
-//The direct code that is called all the time 
+//The direct code that is called all the time
 makeArtists(databaseOfArtist);
 clickAddNewArtist();
-clickShowAllAndFilter();
+clickResetAndFilter();
